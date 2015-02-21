3900a0c31e499329c34b01c8566cd6262fcf7b19
Kevin Rio <kevinwrio@gmail.com>
2015-02-21 12:57:42 -0500

    Aesthetic changes to instantaneous delay plot
    Tweaked the aesthetics of the instantaneous delay plot. Specifically,
    rendered 'leading' and 'following' helmets as green and red,
    respectively. Other minor changes, e.g. enlarging the center helmet and
    changing fonts.

--- plotInstaneousDelays.m
+++ plotInstaneousDelays.m
@@ -21,23 +21,21 @@ trial = 1;
 time = 3600;
 
 % Initialization.
-cHelm = Quant(trial).ctrHelm(time);
 window = 30;
 begTime = time-window;
 endTime = time+window;
-
-% Main loop.
+cHelm = Quant(trial).ctrHelm(time);
 cMax = NaN(nHelm,1); % correlation in heading
 rMax = NaN(nHelm,1); % correlation in speed
 tMax = NaN(nHelm,2); % time delay
+
+% Compute cross-correlation.
 for iHelm = 1:nHelm
-    
-    % If current helmet equals the center helmet, return NaN's. 
+    % If current helmet equals the center helmet, return NaN's.
     if iHelm == cHelm
         cMax(iHelm,1) = NaN;
         rMax(iHelm,1) = NaN;
         tMax(iHelm,1:2) = NaN;
-    
     % Compute correlation in speed and heading, and optimal delays.
     else
         hdnI = Traj(trial,iHelm).hdn;
@@ -45,59 +43,64 @@ for iHelm = 1:nHelm
         [cMax(iHelm,1),tMax(iHelm,1)] = corrHdn(hdnI,hdnJ,begTime,endTime);
         [rMax(iHelm,1),tMax(iHelm,2)] = corrSpd(hdnI,hdnJ,begTime,endTime);
     end
-    
 end
 
-% Plot current position and heading for all helmets.
+% Record the current position of the center helmet.
 x0 = Traj(trial,cHelm).x(time);
 y0 = Traj(trial,cHelm).y(time);
-for iHelm = 1:nHelm
-    
-    % Read in x, y, and heading. 
-    x = Traj(trial,iHelm).x(time)-x0;
-    y = Traj(trial,iHelm).y(time)-y0;
-    hdn = Traj(trial,iHelm).hdn(time);
 
+% Plot subtle lines connecting 'leading' helmets to center helmet.
+for iHelm = 1:nHelm
+    % Read in x, y, and heading.
+    x = Traj(trial,iHelm).x(time);
+    y = Traj(trial,iHelm).y(time);
+    hdn = Traj(trial,iHelm).hdn(time);
+    % Draw line.
+    if iHelm ~= cHelm && mean(tMax(iHelm,:))/60 < 0
+        plot([x x0],[y y0],'Color',[0.85 1 0.85],'LineWidth',5); hold on;
+    end
+end
+
+% Plot position and heading of all helmets.
+for iHelm = 1:nHelm
+    % Read in x, y, and heading.
+    x = Traj(trial,iHelm).x(time);
+    y = Traj(trial,iHelm).y(time);
+    hdn = Traj(trial,iHelm).hdn(time);
     % Plot center helmet (big and bold).
     if iHelm == cHelm
         xP = [x,x+0.6*cos(hdn)];
         yP = [y,y+0.6*sin(hdn)];
-        plot(x,y,'.','Color',[0 0.8 0],'MarkerSize',72); hold on;
-        plot(xP,yP,'Color',[0 0.8 0],'LineWidth',8); hold on;
-    
+        plot(x,y,'.','Color',[0.3 0.3 0.3],'MarkerSize',100); hold on;
+        plot(xP,yP,'Color',[0.3 0.3 0.3],'LineWidth',12); hold on;
     % Plot other helmets.
     else
         if isnan(cMax(iHelm,1)) == 0
-            
-            % Plot helmet and heading. 
-            xP = [x,x+0.3*cos(hdn)];
-            yP = [y,y+0.3*sin(hdn)];
-            plot(x,y,'.','Color',[0.3 1 0.3],'MarkerSize',36); hold on;
-            plot(xP,yP,'Color',[0.3 1 0.3],'LineWidth',3); hold on;
-            
-            % Display cMax, rMax, and tMax next to each helmet.
-            space = 0.1;
-            r = num2str(rMax(iHelm,1));
-            c = num2str(cMax(iHelm,1));
-            t = num2str((mean(tMax(iHelm,:)))/60); % avg of spd and hdn
-            % t = num2str(tMax(iHelm,1)/60); % computed with heading only
+            xP = [x,x+0.25*cos(hdn)];
+            yP = [y,y+0.25*sin(hdn)];
+            % Leaders in green.
+            if mean(tMax(iHelm,:))/60 >= 0
+                plot(x,y,'.','Color',[1 0.3 0.3],'MarkerSize',36); hold on;
+                plot(xP,yP,'Color',[1 0.3 0.3],'LineWidth',3); hold on;
+            % Followers in red.
+            elseif mean(tMax(iHelm,:))/60 < 0
+                plot(x,y,'.','Color',[0.3 1 0.3],'MarkerSize',36); hold on;
+                plot(xP,yP,'Color',[0.3 1 0.3],'LineWidth',3); hold on;
+            end
+            % Time delay.
+            space = 0.05;
+            t = num2str(-mean(tMax(iHelm,:))/60);
             if length(t) < 4
-                text(x+0.1,y-2*space,['t = ',t,' s'],'FontSize',8);
+                text(x+space,y-2*space,['t = ',t,' s'],'FontSize',10,'FontName', 'CMU Bright');
             else
-                text(x+0.1,y-2*space,['t = ',t(1:4),' s'],'FontSize',8);
+                text(x+space,y-2*space,['t = ',t(1:4),' s'],'FontSize',10,'FontName', 'CMU Bright');
             end
-            % text(x+0.1,y,['r = ',r(1:4)],'FontSize',8); % display r
-            % text(x+0.1,y-space,['C = ',c(1:4)],'FontSize',8); % display C
-            
-            % Draw red lines connecting neighbors with negative delays.
-            if mean(tMax(iHelm,:))/60 < 0
-                plot([x 0],[y 0],'r'); hold on;
-            end
- 
         end
     end
 end
 
-% Add axis labels.
-xlabel('x (m)')
-ylabel('y (m)')
+% Aesthetics.
+set(gca, 'FontName', 'CMU Bright','FontSize',12)
+xlabel('x (m)','FontName','CMU Sans Serif','FontSize',20,'FontWeight','Bold')
+ylabel('y (m)','FontName','CMU Bright','FontSize',20,'FontWeight','Bold')
+
